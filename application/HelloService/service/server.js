1060d565e54d8077dd62eb401da4e8f8a8a01f5e
Chris Price <cprice404@fastmail.com>
2024-04-18 06:21:28 -0700

    Hello service is now calling time service, just need to figure out addresses

--- application/HelloService/service/server.js
+++ application/HelloService/service/server.js
@@ -1,9 +1,13 @@
 const express = require('express')
+const fetch = require('node-fetch');
+
 const app = express()
 const port = 8080
 
 app.get('/', (req, res) => {
-  res.send("Hello there!");
+  fetch('http://52.148.144.7:8080')
+    .then(timeServerResponse => timeServerResponse.text())
+    .then(body => res.send(`Hello there! The time is: ${JSON.parse(body).time}`));
 });
 
 app.listen(port, () => {

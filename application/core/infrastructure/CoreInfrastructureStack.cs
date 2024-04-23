3b1ea22978c6b892f1d4c2f2b4d348eb8ca59d29
Chris Price <cprice404@fastmail.com>
2024-04-23 16:04:40 -0700

    Set up AppConfig to allow us to store kube ids in config

--- application/core/infrastructure/CoreInfrastructureStack.cs
+++ application/core/infrastructure/CoreInfrastructureStack.cs
@@ -1,8 +1,12 @@
+using System.Collections.Generic;
 using AzureCdktfUtils;
 using CellRegistry;
 using Constructs;
 using HashiCorp.Cdktf;
+using HashiCorp.Cdktf.Providers.Azurerm.AppConfiguration;
+using HashiCorp.Cdktf.Providers.Azurerm.AppConfigurationKey;
 using HashiCorp.Cdktf.Providers.Azurerm.ContainerRegistry;
+using HashiCorp.Cdktf.Providers.Azurerm.DataAzurermClientConfig;
 using HashiCorp.Cdktf.Providers.Azurerm.KubernetesCluster;
 using HashiCorp.Cdktf.Providers.Azurerm.ResourceGroup;
 using HashiCorp.Cdktf.Providers.Azurerm.RoleAssignment;
@@ -77,5 +81,70 @@ class CoreInfrastructureStack : TerraformStack
             RoleDefinitionName = "AcrPull",
             Scope = containerRegistry.Id
         });
+
+        var appConfigStore = new AppConfiguration(this, "azure-cellular-demo-appconfig3", new AppConfigurationConfig()
+        {
+            Name = "azure-cellular-demo-appconfig3",
+            ResourceGroupName = rg.Name,
+            Location = options.Location
+        });
+
+        var appConfigDataOwner = new RoleAssignment(this, "azure-cellular-demo-appconfig-dataowner",
+            new RoleAssignmentConfig()
+            {
+                PrincipalId = new DataAzurermClientConfig(this, "azure-client-config",
+                    new DataAzurermClientConfigConfig()
+                    {
+
+                    }).ObjectId,
+                RoleDefinitionName = "App Configuration Data Owner",
+                Scope = appConfigStore.Id
+            });
+
+        new AppConfigurationKey(this, "azure-cellular-demo-appconfig-samplekey", new AppConfigurationKeyConfig()
+        {
+            ConfigurationStoreId = appConfigStore.Id,
+            Key = "samplekey",
+            Value = "samplevalue",
+            Timeouts = new AppConfigurationKeyTimeouts()
+            {
+                Create = "1m",
+                Delete = "1m",
+                Update = "1m"
+            },
+            DependsOn = (new List<ITerraformDependable>() {appConfigDataOwner}).ToArray()
+        });
+        /*
+         * 
+           
+           resource "azurerm_role_assignment" "appconf_dataowner" {
+             scope                = azurerm_app_configuration.appconfiguration.id
+             role_definition_name = "App Configuration Data Owner"
+             principal_id         = var.SP_OBJECT_ID
+           }
+           
+           resource "azurerm_app_configuration_key" "settings" {
+             configuration_store_id = azurerm_app_configuration.appconfiguration.id
+             key                    = "Compensation:Settings"
+           
+             depends_on = [
+               azurerm_role_assignment.appconf_dataowner
+             ]
+           }
+         */
+
+        /*
+         *
+           resource "azurerm_app_configuration_key" "test" {
+             configuration_store_id = azurerm_app_configuration.appconf.id
+             key                    = "appConfKey1"
+             label                  = "somelabel"
+             value                  = "a test"
+
+             depends_on = [
+               azurerm_role_assignment.appconf_dataowner
+             ]
+           }
+         */
     }
 }

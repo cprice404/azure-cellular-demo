459e0b542ae50d39b953ba1df3877253dc2aad1f
Chris Price <cprice404@fastmail.com>
2024-04-29 20:57:09 -0700

    POP: adding steps for generating yml

--- Pipelines/PipelinesGenerator/PipelinesGenerator.cs
+++ Pipelines/PipelinesGenerator/PipelinesGenerator.cs
@@ -4,7 +4,7 @@ public class PipelinesGenerator
 {
     public void GeneratePipelines()
     {
-        string outputDirectory = Path.Join(Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.Parent.FullName, "generated-pipeline-yml/");
+        string outputDirectory = Path.Join(Directory.GetParent(Directory.GetCurrentDirectory()).FullName, "generated-pipeline-yml/");
         Directory.CreateDirectory(outputDirectory);
         foreach (var applicationComponent in ApplicationComponents.AllComponents)
         {

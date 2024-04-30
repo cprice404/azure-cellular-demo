bee213c13e2518b5359d83d7ba3204c8e08b09a2
Chris Price <cprice404@fastmail.com>
2024-04-29 20:22:41 -0700

    POP: adding steps for generating yml

---
+++ Pipelines/PipelinesGenerator/PipelinesGenerator.cs
@@ -0,0 +1,10 @@
+namespace PipelinesGenerator;
+
+public class PipelinesGenerator
+{
+    public void GeneratePipelines()
+    {
+        // Generate pipelines
+        Console.WriteLine("Hi! I should be generating pipelines now.");
+    }
+}
